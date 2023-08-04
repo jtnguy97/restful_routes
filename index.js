69a5fc95b201876aee0a9f4e2135616bb548f3b9
jtnguy29 <jtnguy29@uno.edu>
2023-08-04 17:28:38 -0500

    comments finished, TODO: express redirects

--- index.js
+++ index.js
@@ -1,8 +1,49 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.use(express.json());
+
+//parse data from form
 app.use(express.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+const comments = [
+    {
+        username: "Todd",
+        comment: "LOL that is so funny"
+    },
+    {
+        username: "Mei",
+        comment: "omgggg"
+    },
+    {
+        username: "Bee",
+        comment: "sdkjfhskdfjhsdf"
+    },
+    {
+        username: "Pauline",
+        comment: "LMAO"
+    }
+]
+
+//index.ejs
+app.get('/comments', (req, res) => {
+    res.render('comments/index', {comments})
+})
+
+//get the form -> new.ejs
+app.get('/comments/new', (req, res) => {
+    res.render('comments/new');
+})
+
+app.post("/comments", (req, res) => {
+    const {username, comment} = req.body;
+    comments.push({username, comment});
+    res.send("It worked");
+})
+
 
 app.get('/tacos', (req, res) => {
     res.send("GET /tacos response");
